864fbbb5d73ba52a41010e2031a5c377554cc8df
alsalazargar_meli <alejandro.salazargarcia@mercadolibre.com.co>
2025-02-27 16:48:41 -0500

    feat: se implementa mock del service de employee

--- mocks/internal_/domain/service/employee_service_mock.go
+++ mocks/internal_/domain/service/employee_service_mock.go
@@ -1,1 +1,43 @@
 package service
+
+import (
+	"github.com/D-Sorrow/meli-frescos/internal/domain/models"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockEmployeeService struct {
+	mock.Mock
+}
+
+func (m *MockEmployeeService) GetEmployees() ([]models.Employee, error) {
+	args := m.Called()
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).([]models.Employee), args.Error(1)
+}
+
+func (m *MockEmployeeService) GetEmployeeById(id int) (models.Employee, error) {
+	args := m.Called(id)
+	return args.Get(0).(models.Employee), args.Error(1)
+}
+
+func (m *MockEmployeeService) CreateEmployee(employee models.Employee) (models.Employee, error) {
+	args := m.Called(employee)
+	return args.Get(0).(models.Employee), args.Error(1)
+}
+
+func (m *MockEmployeeService) UpdateEmployee(employeeId int, employee models.EmployeePatchRequest) (models.Employee, error) {
+	args := m.Called(employeeId, employee)
+	return args.Get(0).(models.Employee), args.Error(1)
+}
+
+func (m *MockEmployeeService) DeleteEmployee(id int) error {
+	args := m.Called(id)
+	return args.Error(0)
+}
+
+func (m *MockEmployeeService) GetReportInboundOrdersByEmployee(employeeId string) ([]models.EmployeeReportInboundOrders, error) {
+	args := m.Called(employeeId)
+	return args.Get(0).([]models.EmployeeReportInboundOrders), args.Error(1)
+}
