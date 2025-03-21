84feb3a0c29123035336769fe85b5f467bd72ea9
sebestrada_meli <sebastian.estrada@mercadolibre.com.co>
2025-03-21 16:12:11 -0500

    test: add unit test for carrier service

---
+++ mocks/internal_/domain/models/carrier_data_mock.go
@@ -0,0 +1,40 @@
+package models
+
+import (
+	carrierModels "github.com/D-Sorrow/meli-frescos/internal/domain/models"
+)
+
+var Carriers = []carrierModels.Carrier{
+	{
+		Id:          1,
+		Cid:         "#086",
+		CompanyName: "Livetube",
+		Address:     "Room 192",
+		Telephone:   "374-776-3015",
+		LocalityId:  1,
+	},
+	{
+		Id:          2,
+		Cid:         "#11e",
+		CompanyName: "Feedbug",
+		Address:     "Apt 308",
+		Telephone:   "495-573-9329",
+		LocalityId:  2,
+	},
+}
+
+// [{
+// 	"id": 1,
+// 	"cid": "#086",
+// 	"company_name": "Livetube",
+// 	"address": "Room 192",
+// 	"telephone": "374-776-3015",
+// 	"locality_id": 1
+//   }, {
+// 	"id": 2,
+// 	"cid": "#11e",
+// 	"company_name": "Feedbug",
+// 	"address": "Apt 308",
+// 	"telephone": "495-573-9329",
+// 	"locality_id": 2
+//   }]
