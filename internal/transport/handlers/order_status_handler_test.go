ea5d05e24f8f57e8c10d244c60c1d18ac23a1377
Fernando Villamizar <josefernando.villamizar@mercadolibre.com.co>
2025-03-26 18:53:35 -0500

    ✅ test(order-status): refactor tests and achieve 100% coverage
    - Refactored `order_status_handler_test.go`, reaching full test coverage.
    - Replaced `getOrderStatusResult` with `switchOrderStatusTest` for modularity.
    - Removed redundant struct and helper functions, improving test maintainability.
    - Separated test cases by endpoint for better organization.

--- internal/transport/handlers/order_status_handler_test.go
+++ internal/transport/handlers/order_status_handler_test.go
@@ -1,118 +1,57 @@
 package handlers_test
 
 import (
-	"bytes"
-	"encoding/json"
-	"io"
+	"context"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/stretchr/testify/assert"
 
 	"github.com/D-Sorrow/meli-frescos/internal/domain/models"
 	"github.com/D-Sorrow/meli-frescos/internal/domain/ports/service"
 	"github.com/D-Sorrow/meli-frescos/internal/transport/handlers"
 	"github.com/D-Sorrow/meli-frescos/internal/transport/handlers/dto"
+	"github.com/D-Sorrow/meli-frescos/mocks/helpers"
 	service_mock "github.com/D-Sorrow/meli-frescos/mocks/internal_/domain/service"
 )
 
-type OrderStatusGenericStruct[T interface{}] struct {
-	Code int    `json:"code"`
-	Msg  string `json:"message"`
-	Data T      `json:"data,omitempty"`
-}
-
-func getOrderStatusResult(
+func switchOrderStatusTest(
 	t *testing.T,
+	test helpers.HandlerTestStruct,
 	rt *chi.Mux,
-	path string,
-	httpMethod string,
-	httpBody map[string]interface{},
-) (result *http.Response) {
+	orderStatusHandler *handlers.OrderStatusHandler,
+) {
 	t.Helper()
+	ctx := context.Background()
 
-	var req *http.Request
-
-	if httpBody != nil {
-		jsonBody, err := json.Marshal(httpBody)
-		if err != nil {
-			t.Fatalf("JSON serialization error: %v", err)
-		}
-
-		req = httptest.NewRequest(httpMethod, path, bytes.NewBuffer(jsonBody))
-	} else {
-		req = httptest.NewRequest(httpMethod, path, nil)
+	switch test.ServiceMethod {
+	case "GetAll":
+		rt.Get("/api/v1/orderStatus", orderStatusHandler.GetAll(&ctx))
+		result := helpers.GetResult(t, rt, test.HttpPath, test.HttpMethod, nil)
+		helpers.CheckHandlerResponse[[]dto.OrderStatusDTO](t, test, result)
 	}
-
-	rec := httptest.NewRecorder()
-	rt.ServeHTTP(rec, req)
-	result = rec.Result()
-	return
 }
 
-func getOrderStatusGenericStruct[T interface{}](
+func assertOrderStatusHandler(
 	t *testing.T,
-	body io.ReadCloser,
-) (responseDTOGenericStruct T) {
-	t.Helper()
-
-	bodyBytes, err := io.ReadAll(body)
-	if err != nil {
-		t.Fatal("Body reading error:", err)
-	}
-	defer body.Close()
-
-	if err := json.Unmarshal(bodyBytes, &responseDTOGenericStruct); err != nil {
-		t.Fatal("JSON parsing error:", err)
-	}
-
-	return
-}
-
-func assertOrderStatusResponse[T interface{}](
-	t *testing.T,
-	responseDTO OrderStatusGenericStruct[T],
-	responseStatusCode int,
-	expectedStatusCode int,
-	expectedOutput interface{},
+	test helpers.HandlerTestStruct,
+	mockService *service_mock.MockOrderStatusService,
 ) {
 	t.Helper()
 
-	assert.Equal(t, expectedStatusCode, responseStatusCode, "HTTP Status Code mismatch")
-
-	if expectedOutput != nil {
-		expectedDTO, ok := expectedOutput.(OrderStatusGenericStruct[T])
-		if !ok {
-			t.Fatal("Failed to convert expectedOutput to OrderStatusGenericStruct")
-		}
-
-		assert.Equal(t, expectedDTO.Code, responseDTO.Code, "ResponseDTO Code mismatch")
-		assert.Equal(t, expectedDTO.Msg, responseDTO.Msg, "ResponseDTO Message mismatch")
-		assert.Equal(t, expectedDTO.Data, responseDTO.Data, "ResponseDTO Data mismatch")
-	}
+	rt := chi.NewRouter()
+	orderStatusHandler := handlers.NewOrderStatusHandler(mockService)
+	switchOrderStatusTest(t, test, rt, orderStatusHandler)
 }
 
-func TestOrderStatusHandler(t *testing.T) {
-	tests := []struct {
-		name               string
-		serviceMethod      string
-		httpPath           string
-		httpMethod         string
-		httpBody           map[string]interface{}
-		mockParams         []interface{}
-		mockResponse       interface{}
-		mockError          error
-		expectedOutput     interface{}
-		expectedStatusCode int
-	}{
+func TestOrderStatusGetAllHandler(t *testing.T) {
+	tests := []helpers.HandlerTestStruct{
 		{
-			name:          "[GetAll] OK Get all order statuses",
-			serviceMethod: "GetAll",
-			httpPath:      "/api/v1/orderStatus",
-			httpMethod:    "GET",
-			mockResponse: []models.OrderStatus{
+			Name:          "[GetAll] OK Get all order statuses",
+			ServiceMethod: "GetAll",
+			HttpPath:      "/api/v1/orderStatus",
+			HttpMethod:    "GET",
+			MockResponse: []models.OrderStatus{
 				{
 					ID: 1,
 					OrderStatusAttributes: models.OrderStatusAttributes{
@@ -126,7 +65,7 @@ func TestOrderStatusHandler(t *testing.T) {
 					},
 				},
 			},
-			expectedOutput: OrderStatusGenericStruct[[]dto.OrderStatusDTO]{
+			ExpectedOutput: helpers.TestGenericStruct[[]dto.OrderStatusDTO]{
 				Code: http.StatusOK,
 				Msg:  "Get all order statuses successful",
 				Data: []dto.OrderStatusDTO{
@@ -140,71 +79,53 @@ func TestOrderStatusHandler(t *testing.T) {
 					},
 				},
 			},
-			expectedStatusCode: http.StatusOK,
+			ExpectedStatusCode: http.StatusOK,
+			ExpectedCalls:      1,
 		},
 		{
-			name:          "[GetAll] Error No order statuses registered yet",
-			serviceMethod: "GetAll",
-			httpPath:      "/api/v1/orderStatus",
-			httpMethod:    "GET",
-			mockResponse:  make([]models.OrderStatus, 0),
-			mockError:     service.ErrOrderStatusNoRegisteredOrderStatusesYet,
-			expectedOutput: OrderStatusGenericStruct[[]dto.OrderStatusDTO]{
+			Name:          "[GetAll] Error No order statuses registered yet",
+			ServiceMethod: "GetAll",
+			HttpPath:      "/api/v1/orderStatus",
+			HttpMethod:    "GET",
+			MockResponse:  make([]models.OrderStatus, 0),
+			MockError:     service.ErrOrderStatusNoRegisteredOrderStatusesYet,
+			ExpectedOutput: helpers.TestGenericStruct[[]dto.OrderStatusDTO]{
 				Code: http.StatusOK,
 				Msg:  "ERR: No registered order statuses yet",
 				Data: nil,
 			},
-			expectedStatusCode: http.StatusOK,
+			ExpectedStatusCode: http.StatusOK,
+			ExpectedCalls:      1,
 		},
 		{
-			name:          "[GetAll] Error Unexpected error",
-			serviceMethod: "GetAll",
-			httpPath:      "/api/v1/orderStatus",
-			httpMethod:    "GET",
-			mockResponse:  make([]models.OrderStatus, 0),
-			mockError:     service.ErrOrderStatusUnexpectedError,
-			expectedOutput: OrderStatusGenericStruct[[]dto.OrderStatusDTO]{
+			Name:          "[GetAll] Error Unexpected error",
+			ServiceMethod: "GetAll",
+			HttpPath:      "/api/v1/orderStatus",
+			HttpMethod:    "GET",
+			MockResponse:  make([]models.OrderStatus, 0),
+			MockError:     service.ErrOrderStatusUnexpectedError,
+			ExpectedOutput: helpers.TestGenericStruct[[]dto.OrderStatusDTO]{
 				Code: http.StatusInternalServerError,
 				Msg:  "ERR: An unexpected error occurred while processing the requested order status, please try again later",
 				Data: nil,
 			},
-			expectedStatusCode: http.StatusInternalServerError,
+			ExpectedStatusCode: http.StatusInternalServerError,
+			ExpectedCalls:      1,
 		},
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
 			mockService := new(service_mock.MockOrderStatusService)
-
-			if tt.mockResponse != nil {
-				mockService.On(tt.serviceMethod, tt.mockParams...).
-					Return(tt.mockResponse, tt.mockError)
-			} else {
-				mockService.On(tt.serviceMethod, tt.mockParams...).Return(tt.mockError)
-			}
-
-			rt := chi.NewRouter()
-			orderStatusHandler := handlers.NewOrderStatusHandler(mockService)
-
-			switch tt.serviceMethod {
-			case "GetAll":
-				rt.Get("/api/v1/orderStatus", orderStatusHandler.GetAll())
-
-				result := getOrderStatusResult(t, rt, tt.httpPath, tt.httpMethod, nil)
-				responseDTO := getOrderStatusGenericStruct[OrderStatusGenericStruct[[]dto.OrderStatusDTO]](
-					t,
-					result.Body,
-				)
-				assertOrderStatusResponse(
-					t,
-					responseDTO,
-					result.StatusCode,
-					tt.expectedStatusCode,
-					tt.expectedOutput,
-				)
-			}
+			helpers.InitServiceMock(t, test, &mockService)
+			assertOrderStatusHandler(t, test, mockService)
 
 			mockService.AssertExpectations(t)
+			mockService.AssertNumberOfCalls(
+				t,
+				test.ServiceMethod,
+				test.ExpectedCalls,
+			)
 		})
 	}
 }
