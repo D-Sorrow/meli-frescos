38c795d5a31b3912ac5ab149cf26434a7e97c444
Fernando Villamizar <josefernando.villamizar@mercadolibre.com.co>
2025-03-26 19:17:11 -0500

    ✨ feat(middleware): add LogErrorMiddleware for error logging
    - Implemented `LogErrorMiddleware` to capture and log errors from HTTP requests.
    - Uses `LogService` to register error logs in the database.
    - Ensures error tracking and monitoring in request handling.

---
+++ internal/transport/middlewares/log_error_middleware.go
@@ -0,0 +1,69 @@
+package middlewares
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/D-Sorrow/meli-frescos/internal/domain/models"
+	"github.com/D-Sorrow/meli-frescos/internal/domain/service"
+	"github.com/D-Sorrow/meli-frescos/internal/infrastructure/db"
+	"github.com/D-Sorrow/meli-frescos/internal/infrastructure/repository"
+)
+
+type contextKey string
+
+const AppErrorKey contextKey = "APP_ERR"
+
+type ResponseRecorder struct {
+	http.ResponseWriter
+	StatusCode int
+}
+
+func NewResponseRecorder(w http.ResponseWriter) *ResponseRecorder {
+	return &ResponseRecorder{
+		ResponseWriter: w,
+		StatusCode:     http.StatusOK,
+	}
+}
+
+func (rr *ResponseRecorder) WriteHeader(statusCode int) {
+	rr.StatusCode = statusCode
+	rr.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (rr *ResponseRecorder) Write(b []byte) (int, error) {
+	return rr.ResponseWriter.Write(b)
+}
+
+func LogErrorMiddleware(
+	db *db.DataBase,
+	ctx *context.Context,
+) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			rr := NewResponseRecorder(w)
+			next.ServeHTTP(rr, r)
+
+			if err := (*ctx).Value(AppErrorKey); err != nil {
+				logRepo := repository.NewLogRepository(db.Db)
+				logService := service.NewLogService(logRepo)
+
+				logAttributes := models.LogAttributes{
+					Level:  models.ERROR,
+					Source: r.URL.Path,
+					Detail: fmt.Sprintf(
+						"Error occurred with status code [%d], %v",
+						rr.StatusCode,
+						err,
+					),
+				}
+
+				if _, err := logService.Register(logAttributes); err != nil {
+					log.Println("Error al registrar el log:", err)
+				}
+			}
+		})
+	}
+}
